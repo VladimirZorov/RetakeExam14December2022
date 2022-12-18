07b07652f23240f17fd39f7b6fa2623c544346af
Vladimir Zorov <vl.zorov@gmail.com>
2022-12-18 19:12:46 +0200

    Add

--- src/NavyBattle.java
+++ src/NavyBattle.java
@@ -4,6 +4,92 @@ public class NavyBattle {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int n = Integer.parseInt(scanner.nextLine());
+
+        String[][] battlefield = new String[n][n];
+        int mineHit = 0;
+        int batCruisersHit = 0;
+        int submarineRow = -1;
+        int submarineColl = -1;
+
+        for (int i = 0; i < n; i++) {
+            String[] line = scanner.nextLine().split("");
+            for (int j = 0; j < n; j++) {
+                if (line[j].equals("S")) {
+                    submarineRow = i;
+                    submarineColl = j;
+                }
+            }
+            battlefield[i] = line;
+        }
+
+        battlefield[submarineRow][submarineColl] = "-";
+
+        String command = scanner.nextLine();
+        while (mineHit < 3 && batCruisersHit < 3) {
+            switch (command){
+                case "up":
+                    submarineRow -= 1;
+                    if (battlefield[submarineRow][submarineColl].equals("*")){
+                        mineHit ++;
+                        battlefield[submarineRow][submarineColl] = "-";
+                    } else if (battlefield[submarineRow][submarineColl].equals("C")){
+                        batCruisersHit ++;
+                        battlefield[submarineRow][submarineColl] = "-";
+                    }
+                    break;
+                case "down":
+                    submarineRow += 1;
+                    if (battlefield[submarineRow][submarineColl].equals("*")){
+                        mineHit ++;
+                        battlefield[submarineRow][submarineColl] = "-";
+                    } else if (battlefield[submarineRow][submarineColl].equals("C")){
+                        batCruisersHit ++;
+                        battlefield[submarineRow][submarineColl] = "-";
+                    }
+                    break;
+                case "left":
+                    submarineColl -= 1;
+                    if (battlefield[submarineRow][submarineColl].equals("*")){
+                        mineHit ++;
+                        battlefield[submarineRow][submarineColl] = "-";
+                    } else if (battlefield[submarineRow][submarineColl].equals("C")){
+                        batCruisersHit ++;
+                        battlefield[submarineRow][submarineColl] = "-";
+                    }
+                    break;
+                case "right":
+                    submarineColl += 1;
+                    if (battlefield[submarineRow][submarineColl].equals("*")){
+                        mineHit ++;
+                        battlefield[submarineRow][submarineColl] = "-";
+                    } else if (battlefield[submarineRow][submarineColl].equals("C")){
+                        batCruisersHit ++;
+                        battlefield[submarineRow][submarineColl] = "-";
+                    }
+                    break;
+            }
+            command = scanner.nextLine();
+        }
+
+        battlefield[submarineRow][submarineColl] = "S";
+        if (batCruisersHit == 3) {
+            System.out.println("Mission accomplished, U-9 has destroyed all battle cruisers of the enemy!%n");
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    System.out.print(battlefield[i][j]);
+                }
+                System.out.println();
+            }
+        } else {
+            System.out.printf("Mission failed, U-9 disappeared! Last known coordinates [%d, %d]!%n", submarineRow, submarineColl);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    System.out.print(battlefield[i][j]);
+                }
+                System.out.println();
+            }
+        }
 
     }
 }
